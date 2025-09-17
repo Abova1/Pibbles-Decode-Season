d2364a12291914397e1697b21b87885613e4308d
myleshill1 <myleshill@gmail.com>
2025-09-17 19:12:36 -0400

    added limelight apriltag stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Limelight/BlueGoalAprilTagLimelight.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.vision.Limelight;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class BlueGoalAprilTagLimelight extends OpMode {
+
+    private Limelight3A limelight;
+
+    /*
+    pipeline 1 is red goal
+    pipeline 2 is blue goal
+    pipeline 3 is motif
+     */
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
+        limelight.pipelineSwitch(2);
+    }
+
+    @Override
+    public void start() {
+        limelight.start();
+
+    }
+
+    @Override
+    public void loop() {
+        LLStatus status = limelight.getStatus();
+        telemetry.addData("Name", "%s",
+                status.getName());
+        telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
+                status.getTemp(), status.getCpu(), (int) status.getFps());
+        telemetry.addData("Pipeline", "Index: %d, Type: %s",
+                status.getPipelineIndex(), status.getPipelineType());
+
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+    }
+}
