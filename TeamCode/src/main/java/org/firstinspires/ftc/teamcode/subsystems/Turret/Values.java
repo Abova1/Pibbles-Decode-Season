b915894110fb9f3885a9ece23dc0367872922a4d
Abova1 <Johngmod7@gmail.com>
2025-09-24 17:05:26 -0400

    More turret updates (All Pseudo code)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret/Values.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret/Values.java
@@ -6,6 +6,6 @@ class Values {
 
     public static double p = 0.0025, i = 0, d = 0.000006;
     public static double GearRatio = 0;
-    public static double TPR = Globals.MOTOR_TICKS.RPM_1620.ordinal(); //PPR but I don't know anymore
+    public static double TPR = Globals.MOTOR_TICKS.RPM_1620.ticksPerRev; //PPR but I don't know anymore
 
 }
